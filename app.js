1437974c5f7ebee9d0392ee1ad2f9c11e559b488
gustavo domingues <deathdiec@gmail.com>
2023-05-17 14:49:07 -0300

    feat: portal texture and sound

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
-var text = document.getElementById("cam_pos");
+//Carregando o asset de som:
+var portalSound = new Audio("portal.wav");
 
 function start() {
-    console.log("adding stuff to the aframe");
-    AFRAME.registerComponent("camera-listener", {
-        tick: function () {
-            var cameraEl = this.el.sceneEl.camera.el;
-            cameraEl.getAttribute("position");
-            cameraEl.getAttribute("rotation");
+    AFRAME.registerComponent("rotation-reader", {
+        /**
+         * We use IIFE (immediately-invoked function expression) to only allocate one
+         * vector or euler and not re-create on every tick to save memory.
+         */
+        tick: (function () {
+            var position = new THREE.Vector3();
+            var quaternion = new THREE.Quaternion();
 
-            text.innerHTML =
-                "Camera position: " +
-                cameraEl.getAttribute("position").x +
-                ", " +
-                cameraEl.getAttribute("position").y +
-                ", " +
-                cameraEl.getAttribute("position").z +
-                "<br>";
-            // Do something.
-        },
+            return function () {
+                this.el.object3D.getWorldPosition(position);
+                this.el.object3D.getWorldQuaternion(quaternion);
+
+                if (position.x === 0) {
+                    portalSound.play();
+                }
+                // position and rotation now contain vector and quaternion in world space.
+            };
+        })(),
     });
 }
 
