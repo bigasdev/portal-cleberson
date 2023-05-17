adaa560042d0045f9f596eb9fb67d5e31f80f219
gustavo domingues <deathdiec@gmail.com>
2023-05-17 14:57:50 -0300

    feat: portal position first test

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 //Carregando o asset de som:
 var portalSound = new Audio("portal.wav");
 
+//Funcao pra mudar a posicao da camera
+function changePos(pos) {
+    document
+        .getElementById("camera")
+        .setAttribute("position", { x: pos.x, y: pos.y, z: pos.z });
+}
+
 function start() {
-    AFRAME.registerComponent("rotation-reader", {
+    AFRAME.registerComponent("main-camera", {
         /**
          * We use IIFE (immediately-invoked function expression) to only allocate one
          * vector or euler and not re-create on every tick to save memory.
@@ -12,11 +19,13 @@ function start() {
             var quaternion = new THREE.Quaternion();
 
             return function () {
+                console.log(position);
                 this.el.object3D.getWorldPosition(position);
                 this.el.object3D.getWorldQuaternion(quaternion);
 
-                if (position.x === 0) {
+                if (position.z >= -2.2 && position.z <= -1.8) {
                     portalSound.play();
+                    changePos({ x: 0, y: 1.6, z: 10 });
                 }
                 // position and rotation now contain vector and quaternion in world space.
             };
